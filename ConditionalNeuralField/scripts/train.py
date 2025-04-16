65c6d3938eab9bf3fdd030e3c79a9c973834c7e8
meet <mparikh2@nd.edu>
2025-04-16 13:01:27 -0400

    bug fix for CNF train

--- ConditionalNeuralField/scripts/train.py
+++ ConditionalNeuralField/scripts/train.py
@@ -3,6 +3,20 @@ from os import mkdir
 import sys
 from basicutility import ReadInput as ri
 
+# RI_PARAMS = {
+#     "read_multiple": False,
+#     "hidden_size": 256,
+#     "readin_data_shape": "t h w c",
+#     "batch_shape": "t h w c",
+#     "dims": 2,
+#     "multiGPU": 1,
+#     "NF": {
+#         "name": "SIRENAutodecoder_film",
+#         "kwargs": {"num_hidden_layers": 10, "out_features": 4},
+#     },
+#     "lr": {"nf": 1.0e-4, "latents": 1.0e-5},
+# }
+# ri.TrainParamReader.update_defaults(RI_PARAMS)
 
 from tqdm import tqdm
 import torch
@@ -21,23 +35,16 @@ from functools import partial
 
 
 class basic_set(Dataset):
-    def __init__(self, fois, coord, extra_siren_in = None) -> None:
+    def __init__(self, fois, coord) -> None:
         super().__init__()
         self.fois = fois
-        self.total_samples = fois.shape[0]
-        
         self.coords = coord
 
     def __len__(self):
-        return self.total_samples
+        return self.fois.shape[0]
 
     def __getitem__(self, idx):
-        if hasattr(self, "extra_in"):
-            extra_id = idx % self.fois.shape[1]
-            idb = idx // self.fois.shape[1]
-            return (self.coords, self.extra_in[extra_id]), self.fois[idb, extra_id], idx
-        else:
-            return self.coords, self.fois[idx], idx
+        return self.coords, self.fois[idx], idx
 
 
 class LatentContainer(torch.nn.Module):
@@ -50,10 +57,9 @@ class LatentContainer(torch.nn.Module):
         N_samples,
         N_features,
         dims,
-        # lumped=False
     ):
         super().__init__()
-        self.expand_dims = " ".join(["1" for _ in range(dims)])# if not lumped else "1"
+        self.expand_dims = " ".join(["1" for _ in range(dims)])
         self.expand_dims = f"N f -> N {self.expand_dims} f"
         self.latents = torch.nn.Parameter(
             torch.zeros((N_samples, N_features), dtype=torch.float32)
@@ -97,7 +103,10 @@ class trainer(object):
                 fois = np.load(f"{hyper_para.data_path}")
 
             assert (
-                rearrange(fois, f"{hyper_para.readin_data_shape} -> {hyper_para.readin_data_shape}")
+                rearrange(
+                    fois,
+                    f"{hyper_para.readin_data_shape} -> {hyper_para.readin_data_shape}",
+                )
                 == fois
             ).all(), f"data shape is {tuple(fois.shape)}, which is inconsistant \
             with the fois_shape ({hyper_para.readin_data_shape}) specified in yaml file."
@@ -114,27 +123,12 @@ class trainer(object):
                 assert (
                     hyper_para.NF["out_features"] == fois.shape[-1]
                 ), "NF_out_features is not consistent with fois shape"
-            
-            if hasattr(hyper_para, "extra_siren_in"):
-                assert isinstance(hyper_para.extra_siren_in, int) or len(hyper_para.extra_siren_in) >= 3
-                if isinstance(hyper_para.extra_siren_in, int):
-                    extra_siren_in = torch.linspace(0,1,hyper_para.extra_siren_in)
-                elif len(hyper_para.extra_siren_in) == 3:
-                    extra_siren_in = torch.linspace(*hyper_para.extra_siren_in)
-                elif len(hyper_para.extra_siren_in) > 3:
-                    extra_siren_in = torch.tensor(hyper_para.extra_siren_in)
-                extra_siren_in_flag = True
-            else:
-                extra_siren_in_flag = False
 
             self.spatio_shape = fois.shape[1:-1]
             self.spatio_axis = list(
                 range(fois.ndim if isinstance(fois, np.ndarray) else fois.dim())
             )[1:-1]
-            if extra_siren_in_flag: 
-                self.spatio_shape = self.spatio_shape[1:]
-                self.spatio_axis = self.spatio_axis[:-1]
-                
+
             ###### read data - coordinate ######
             if hasattr(hyper_para, "coor_path"):
                 coord = np.load(f"{hyper_para.coor_path}")
@@ -150,15 +144,14 @@ class trainer(object):
                 )
                 coord = [np.linspace(0, 1, i) for i in self.spatio_shape]
                 coord = np.stack(np.meshgrid(*coord, indexing="ij"), axis=-1)
-                
-            self.train_coord = torch.tensor(coord, dtype=torch.float32)
+
             ###### convert to tensor ######
             fois = (
                 torch.tensor(fois, dtype=torch.float32)
                 if not isinstance(fois, torch.Tensor)
                 else fois
             )
-            self.N_samples = fois.shape[0] if not extra_siren_in_flag else fois.shape[0] * fois.shape[1]
+            self.N_samples = fois.shape[0]
             print(f"total training samples: {self.N_samples}")
             coord = (
                 torch.tensor(coord, dtype=torch.float32)
@@ -169,15 +162,10 @@ class trainer(object):
         ###### normalizer ######
         self.in_normalizer = Normalizer_ts(**hyper_para.normalizer)
         self.out_normalizer = Normalizer_ts(**hyper_para.normalizer)
-        if hasattr(hyper_para, "extra_siren_in"):
-            self.extra_in_normalizer = Normalizer_ts(**hyper_para.normalizer)
-            extra_siren_in_flag = True
-        else:
-            extra_siren_in_flag = False
-            
+
         if not exists(f"{hyper_para.save_path}") and (not infer_mode):
             mkdir(hyper_para.save_path)
-            
+
         if exists(f"{hyper_para.save_path}/normalizer_params.pt"):
             print(
                 f"loading normalizer parameters from {hyper_para.save_path}/normalizer_params.pt"
@@ -185,32 +173,22 @@ class trainer(object):
             norm_params = torch.load(f"{hyper_para.save_path}/normalizer_params.pt")
             self.in_normalizer.params = norm_params["x_normalizer_params"]
             self.out_normalizer.params = norm_params["y_normalizer_params"]
-            if extra_siren_in_flag:
-                self.extra_in_normalizer.params = norm_params["extra_normalizer_params"]
 
         elif not infer_mode:
             print("No noramlization file found! Calculating normalizer parameters")
-            self.in_normalizer.fit_normalize(
-                coord if hyper_para.lumped_latent else coord.flatten(0, hyper_para.dims-1) 
-            )
-            if extra_siren_in_flag:
-                self.out_normalizer.fit_normalize(fois.flatten(0, hyper_para.dims+1))
-            else:
-                self.out_normalizer.fit_normalize(
-                    fois if hyper_para.lumped_latent else fois.flatten(0, hyper_para.dims)
-                )
-            if extra_siren_in_flag:
-                self.extra_in_normalizer.fit_normalize(extra_siren_in.flatten())
+            self.in_normalizer.fit_normalize(coord.flatten(0, -2))
+            self.out_normalizer.fit_normalize(fois.flatten(0, -2))
+
             print(
                 f"Saving normalizer parameters to {hyper_para.save_path}/normalizer_params.pt"
             )
-            toSave = {
-                "x_normalizer_params": self.in_normalizer.get_params(),
-                "y_normalizer_params": self.out_normalizer.get_params(),
-            }
-            if extra_siren_in_flag:
-                toSave["extra_normalizer_params"] = self.extra_in_normalizer.get_params()
-            torch.save(toSave, hyper_para.save_path + "/normalizer_params.pt",)
+            torch.save(
+                {
+                    "x_normalizer_params": self.in_normalizer.get_params(),
+                    "y_normalizer_params": self.out_normalizer.get_params(),
+                },
+                hyper_para.save_path + "/normalizer_params.pt",
+            )
         else:
             raise FileNotFoundError(
                 f"{hyper_para.save_path}/normalizer_params.pt does not exist"
@@ -219,16 +197,11 @@ class trainer(object):
         if not infer_mode:
             normed_coords = self.in_normalizer.normalize(coord)
             normed_fois = self.out_normalizer.normalize(fois)
-            if extra_siren_in_flag:
-                normed_extra_siren_in = self.extra_in_normalizer.normalize(extra_siren_in)
-            else:
-                normed_extra_siren_in = None
-
 
         ###### nf ######
         if "kwargs" not in hyper_para.NF:
             self.nf: torch.nn.Module = getattr(nf_networks, hyper_para.NF["name"])(
-                in_coord_features=hyper_para.dims if not extra_siren_in_flag else hyper_para.dims+1,
+                in_coord_features=hyper_para.dims,
                 in_latent_features=hyper_para.hidden_size,
                 out_features=hyper_para.NF["out_features"],
                 num_hidden_layers=hyper_para.NF["num_hidden_layers"],
@@ -242,7 +215,7 @@ class trainer(object):
         ###### latents ######
         if not infer_mode:
             self.latents = LatentContainer(
-                self.N_samples, hyper_para.hidden_size, hyper_para.dims, hyper_para.lumped_latent
+                self.N_samples, hyper_para.hidden_size, hyper_para.dims
             )
 
         self.hyper_para = hyper_para
@@ -252,7 +225,7 @@ class trainer(object):
                 raise NotImplementedError
 
             else:
-                self.dataset = basic_set(normed_fois, normed_coords, normed_extra_siren_in)
+                self.dataset = basic_set(normed_fois, normed_coords)
 
             self.test_criteria = partial(
                 getattr(sys.modules[__name__], hyper_para.test_criteria),
@@ -267,15 +240,8 @@ class trainer(object):
         coord: torch.Tensor,
         latents: torch.Tensor,
     ) -> torch.Tensor:
-        if coord is None:
-            print("Using default training query points")
-        coord = coord if coord is not None else self.train_coord
         coord = self.in_normalizer.normalize(coord)
-        if len(coord.shape) > 2:
-            latents = latents[:, None, None]
-        else:
-            latents = latents[:, None]
-        out = self.nf(coord.to(latents.device), latents)
+        out = self.nf(coord, latents)
         return self.out_normalizer.denormalize(out)
 
     def train(self, fix_nf = False):
@@ -384,12 +350,13 @@ class trainer(object):
         optim_states = torch.optim.Adam(
             latents.parameters(), lr=hyper_para.lr["latents"]
         )
+        
+        # scheduler_net_dec = torch.optim.lr_scheduler.ExponentialLR(optim_net_dec, gamma=0.999)#torch.optim.lr_scheduler.CosineAnnealingLR(optim_net_dec,T_max=100, eta_min=1e-6)
+        # scheduler_states = torch.optim.lr_scheduler.ExponentialLR(optim_states, gamma=0.999)#torch.optim.lr_scheduler.CosineAnnealingLR(optim_states, T_max=100, eta_min=1e-6)
 
-        for k in optim_dict:
-            if k == "optim_net_dec_dict":
-                optim_net_dec.load_state_dict(optim_dict["optim_net_dec_dict"])
-            elif k == "optim_states_dict":
-                optim_states.load_state_dict(optim_dict["optim_states_dict"])
+        if len(optim_dict) > 0:
+            optim_net_dec.load_state_dict(optim_dict["optim_net_dec_dict"])
+            optim_states.load_state_dict(optim_dict["optim_states_dict"])
         
         if fix_nf: model.eval()
         
@@ -404,12 +371,10 @@ class trainer(object):
             for batch_coords, batch_fois, idx in train_loader:
 
                 batch_latent = latents(idx)
+                batch_coords = batch_coords.to(rank)
                 batch_fois = batch_fois.to(rank)
-                if isinstance(batch_coords, list): 
-                    batch_coords = [i.to(rank) for i in batch_coords]
-                else:
-                    batch_coords = batch_coords.to(rank)
-                batch_output = model(batch_coords, batch_latent)                
+
+                batch_output = model(batch_coords, batch_latent)
                 loss = criterion(batch_output, batch_fois)
                 optim_states.zero_grad()
                 loss.backward()
@@ -417,6 +382,10 @@ class trainer(object):
                 train_ins_error.append(loss)
 
             epoch_loss = torch.stack(train_ins_error).mean()  # .to(rank)
+            
+            # scheduler_net_dec.step()
+            # scheduler_states.step()
+            
             if world_size > 1:
                 torch.distributed.reduce(
                     epoch_loss, op=torch.distributed.ReduceOp.AVG, dst=0
@@ -426,7 +395,7 @@ class trainer(object):
                 tqdm.write(str(mean_loss))
                 logger.add_scalar("loss", mean_loss, i)
 
-            if i % hyper_para.save_every == 0 or i == start_epoch + hyper_para.epochs-1:  # and i > 0:
+            if i % hyper_para.save_every == 0:  # and i > 0:
 
                 test_error = []
                 model.eval()
@@ -435,12 +404,8 @@ class trainer(object):
                     if world_size > 1:
                         test_loader.sampler.set_epoch(i)
                     for test_coords, test_fois, idx in test_loader:
-                        if isinstance(test_coords, list): 
-                            test_coords = [i.to(rank) for i in test_coords]
-                        else:
-                            test_coords = test_coords.to(rank)
                         prediction = out_normalizer.denormalize(
-                            model(test_coords, latents(idx))
+                            model(test_coords.to(rank), latents(idx))
                         )
                         target = out_normalizer.denormalize(test_fois.to(rank))
                         error = test_criteria(prediction, target)
@@ -478,7 +443,7 @@ class trainer(object):
             f"{self.hyper_para.save_path}/checkpoint_{epoch}.pt",
         )
 
-    def load(self, checkpoint_id: int, siren_only=False):
+    def load(self, checkpoint_id: int):
 
         if checkpoint_id == -1:
             import glob
@@ -487,13 +452,7 @@ class trainer(object):
             checkpoint_list = [
                 int(i.split("_")[-1].split(".")[0]) for i in checkpoint_list
             ]
-            try:
-                checkpoint_id = max(checkpoint_list)
-            except ValueError:
-                print(
-                    f"*No checkpoint found in {self.hyper_para.save_path}*, starting from scratch"
-                )
-                return 
+            checkpoint_id = max(checkpoint_list)
 
         print(
             f"loading checkpoint from {self.hyper_para.save_path}/checkpoint_{checkpoint_id}.pt"
@@ -503,29 +462,20 @@ class trainer(object):
             f"{self.hyper_para.save_path}/checkpoint_{checkpoint_id}.pt"
         )
         self.nf.load_state_dict(checkpoint["model_state_dict"])
-
-        self.start_epoch = checkpoint["epoch"]
-
-        if not siren_only:
-            if hasattr(self, "N_samples"):
-                assert self.N_samples == checkpoint["hidden_states"]["latents"].shape[0]
-            else:
-                self.N_samples = checkpoint["hidden_states"]["latents"].shape[0]
-            self.latents = LatentContainer(
-                self.N_samples, self.hyper_para.hidden_size, self.hyper_para.dims, #self.hyper_para.lumped_latent
-            )
-            self.latents.load_state_dict(checkpoint["hidden_states"])
-
-            self.optim_dict = {
-                k: checkpoint[k] for k in ["optim_net_dec_dict", "optim_states_dict"]
-            }
-            
-            return self.nf, self.latents, self.optim_dict, checkpoint["epoch"]
-
+        if hasattr(self, "N_samples"):
+            assert self.N_samples == checkpoint["hidden_states"]["latents"].shape[0]
         else:
-            self.optim_dict = {}
-            self.optim_dict["optim_net_dec_dict"] = checkpoint["optim_net_dec_dict"]
+            self.N_samples = checkpoint["hidden_states"]["latents"].shape[0]
+        self.latents = LatentContainer(
+            self.N_samples, self.hyper_para.hidden_size, self.hyper_para.dims
+        )
+        self.latents.load_state_dict(checkpoint["hidden_states"])
 
+        self.optim_dict = {
+            k: checkpoint[k] for k in ["optim_net_dec_dict", "optim_states_dict"]
+        }
+        self.start_epoch = checkpoint["epoch"]
+        return self.nf, self.latents, self.optim_dict, checkpoint["epoch"]
 
 
 if __name__ == "__main__":
@@ -533,8 +483,8 @@ if __name__ == "__main__":
     import os
 
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12356"
+    os.environ["MASTER_PORT"] = "12355"
     hp = ri.basic_input(sys.argv[1])
     mytrainer = trainer(hp)
-    mytrainer.load(-1)
+    # mytrainer.load(-1)
     mytrainer.train()
